a52b7f3402ae0f990d9c0ad899eebc23da90b730
Patrick Sollberger <psollberger83@gmail.com>
2020-03-22 11:58:25 +0100

    Update ITelSearchClient.cs

--- TelSearchApi/ITelSearchClient.cs
+++ TelSearchApi/ITelSearchClient.cs
@@ -1,7 +1,7 @@
-﻿using System.Threading.Tasks;
-
-namespace TelSearchApi
+﻿namespace TelSearchApi
 {
+  using System.Threading.Tasks;
+
   public interface ITelSearchClient
   {
     string ApiKey { get; }
